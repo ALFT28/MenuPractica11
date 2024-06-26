5732393e658f7cab728881a7916925ec093eaa08
angel <angel@DESKTOP-8ELEIVF>
2024-06-26 15:34:18 -0600

    Imagenes y triangulo

--- MenuPractica11/MainPage.xaml.cs
+++ MenuPractica11/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MenuPractica11
+﻿using _4955511ProgramasP11;
+
+namespace MenuPractica11
 {
     public partial class MainPage : ContentPage
     {
@@ -7,14 +9,22 @@
             InitializeComponent();
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked_4(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Dinosaurio());
+        }
+        private async void Button_Clicked3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Triangulo());
+        }
+        private async void Button_Clicked2(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new CambioCliente());
         }
 
-        private async void Button_Clicked_1(object sender, EventArgs e)
+        private async void Button_Clicked1(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new Dinosaurio());
+            await Navigation.PushAsync(new Gasolinera());
         }
     }
 
