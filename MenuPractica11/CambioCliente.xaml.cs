f132a1e9a344d9f68568eb79b5d3eeaa7d1cd96c
JOEL <JOEL@DESKTOP-HEVUQ5C>
2024-06-25 20:28:39 -0600

    Revert "#Se agrego la funcionalidad a la pagina Cambio Cliente"
    This reverts commit 99ff44d4815c0aeebc049b2f81a399f5226c8fa3.

--- MenuPractica11/CambioCliente.xaml.cs
+++ MenuPractica11/CambioCliente.xaml.cs
@@ -6,34 +6,4 @@ public partial class CambioCliente : ContentPage
 	{
 		InitializeComponent();
 	}
-
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-        if (!String.IsNullOrEmpty(EntryCosto.Text) && !String.IsNullOrEmpty(EntryDinero.Text))
-        {
-            double Costo, Dinero;
-
-            if (double.TryParse(EntryCosto.Text, out Costo) && double.TryParse(EntryDinero.Text, out Dinero))
-            {
-                double Cambio = Dinero - Costo;
-
-                EntryCambio.Text = Cambio.ToString();
-            }
-            else
-            {
-                DisplayAlert("Error", "Solo se acepta numeros", "OK");
-            }
-        }
-        else
-        {
-            DisplayAlert("Error", "Porfavor agregue los numeros", "OK");
-        }
-    }
-
-    private void Button_Clicked_1(object sender, EventArgs e)
-    {
-        EntryCambio.Text = "";
-        EntryCosto.Text = "";
-        EntryDinero.Text = "";
-    }
 }
