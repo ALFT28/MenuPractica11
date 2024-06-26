593620553b9aee661ef0469bb272467503039393
JOEL <JOEL@DESKTOP-HEVUQ5C>
2024-06-25 19:38:22 -0600

    Agregar archivos de proyecto.

---
+++ MenuPractica11/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MenuPractica11
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
