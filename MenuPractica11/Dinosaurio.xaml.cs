48e6decc7242f01757753f63fb98122b4e61e834
JOEL <JOEL@DESKTOP-HEVUQ5C>
2024-06-25 20:45:24 -0600

    #Se agrego la funcionalidad a la pagina Dinosaurio

--- MenuPractica11/Dinosaurio.xaml.cs
+++ MenuPractica11/Dinosaurio.xaml.cs
@@ -6,4 +6,40 @@ public partial class Dinosaurio : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        EntryKg.Text = "";
+        EntryCm.Text = "";
+        EntryMts.Text = "";
+        EntryLb.Text = "";
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        if (!String.IsNullOrEmpty(EntryKg.Text) && !String.IsNullOrEmpty(EntryMts.Text))
+        {
+            double KG, MTS, CM = 100, LB = 2.20462;
+
+            if (double.TryParse(EntryKg.Text, out KG) && double.TryParse(EntryMts.Text, out MTS))
+            {
+
+                double Conversion1 = KG * LB;
+                double Conversion2 = MTS * CM;
+
+                EntryLb.Text = Conversion1.ToString();
+                EntryCm.Text = Conversion2.ToString();
+
+
+            }
+            else
+            {
+                DisplayAlert("Error", "Por favor introduce solo números válidos.", "Ok");
+            }
+        }
+        else
+        {
+            DisplayAlert("Error", "Introduce los numeros", "Ok");
+        }
+    }
 }
