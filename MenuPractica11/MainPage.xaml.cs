4a86bbb0024e1afd38be9ff3ec410ad69c9900ee
JOEL <JOEL@DESKTOP-HEVUQ5C>
2024-06-25 20:49:29 -0600

    #Se modifico el segundo de los botones en el MainPage

--- MenuPractica11/MainPage.xaml.cs
+++ MenuPractica11/MainPage.xaml.cs
@@ -11,6 +11,11 @@
         {
             await Navigation.PushAsync(new CambioCliente());
         }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Dinosaurio());
+        }
     }
 
 }
